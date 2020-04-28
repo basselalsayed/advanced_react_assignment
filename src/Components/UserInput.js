1683443bea10de4d9b0c8c7412e2445f150f83f8
basselalsayed <alsayed.bassel@gmail.com>
2020-04-28 17:43:08 +0100

    created userInput and Output

---
+++ src/Components/UserInput.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const userInput = () => {
+  return (
+    <input />
+  )
+};
+
+export default userInput;
---
+++ src/Components/UserOutput.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+
+const userOutput = () => {
+  return (
+    <Fragment>
+      <p>test 1</p>
+      <p>test 2</p>
+    </Fragment>
+  )
+};
+
+export default userOutput;
